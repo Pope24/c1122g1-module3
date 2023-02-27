53761d8a0d375230c19dd70930f247c917895803
Pope24 <110238804+Pope24@users.noreply.github.com>
2023-02-27 13:29:02 +0700

    up

--- ss3_thao_tac_csdl/bai_tap/query_data_from_student_Data/query_data_from_student_management.sql
+++ ss3_thao_tac_csdl/bai_tap/query_data_from_student_Data/query_data_from_student_management.sql
@@ -15,7 +15,7 @@ create table student (
     class_id int not null,
     foreign key(class_id) references class_of_school(class_id)
 );
-create table mon_hoc (
+create table subject_class (
 	mh_id int auto_increment primary key,
     mh_name varchar(30) not null,
     credit tinyint not null default 1 check (credit >= 1),
@@ -28,7 +28,7 @@ create table mark (
     mark float default 0 check (mark between 0 and 100),
     exam_times tinyint default 1,
     unique(mh_id, student_id),
-    foreign key (mh_id) references mon_hoc(mh_id),
+    foreign key (mh_id) references subject_class(mh_id),
     foreign key (student_id) references student(student_id)
 );
 insert into class_of_school (class_name, start_date, status_class)
@@ -41,12 +41,12 @@ values 	("Hung", "Ha Noi", "0912113113", 1, 1),
 		("Hoa", "Hai Phong", null , 1, 1),
 		("Manh", "HCM", "0123123123", 0, 2);
         select * from student;
-insert into mon_hoc (mh_name, credit, status_mh)
+insert into subject_class (mh_name, credit, status_mh)
 values 	("CF", 5, 1),
 		("C", 6, 1),
         ("HDJ", 5, 1),
         ("RDBMS", 10, 1);
-select * from mon_hoc;
+select * from subject_class;
 insert into mark(mh_id, student_id, mark, exam_times)
 values 	(1,1,8,1),
 		(1,2,10,2),
@@ -60,7 +60,7 @@ select * from student where student.student_name like 'h%';
 select * from class_of_school where month(class_of_school.start_date) = 12;
 
 -- Hiển thị tất cả các thông tin môn học có credit trong khoảng từ 3-5.
-select * from mon_hoc where mon_hoc.credit >= 3 and mon_hoc.credit <=5;
+select * from subject_class where subject_class.credit >= 3 and subject_class.credit <=5;
 
 -- Thay đổi mã lớp(ClassID) của sinh viên có tên ‘Hung’ là 2.
 SET SQL_SAFE_UPDATES = 0;
@@ -69,6 +69,6 @@ SET SQL_SAFE_UPDATES = 1;
 select * from student;
 
 -- Hiển thị các thông tin: StudentName, SubName, Mark. Dữ liệu sắp xếp theo điểm thi (mark) giảm dần. nếu trùng sắp theo tên tăng dần.
-select student.student_name, mon_hoc.mh_name, mark.mark from student 
-inner join mon_hoc inner join mark 
-on (student.student_id = mark.student_id) AND (mark.mh_id = mon_hoc.mh_id) order by mark desc, student.student_name;
+select student.student_name, subject_class.mh_name, mark.mark from student 
+inner join subject_class inner join mark 
+on (student.student_id = mark.student_id) AND (mark.mh_id = subject_class.mh_id) order by mark desc, student.student_name;
